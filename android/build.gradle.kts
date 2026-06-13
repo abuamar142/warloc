feb8b1ac7875218d43aca2d8f3140558c16e9740
Abu Amar <abuamar.albadawi@gmail.com>
2026-06-13 22:35:34 +0700

    fix: resolve Java 8 obsolete compiler warnings in gradle subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,23 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
